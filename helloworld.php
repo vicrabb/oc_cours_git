51473646e38b544fdd23951ee435599146f4ff69
vicrabb <vamorini@gmail.com>
2018-04-17 11:54:57 +0200

    test

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,14 @@
 <?php
+echo '<html>';
+echo '<head><title>Bonjour</title></head>';
+echo '<body>';
+echo '<p>'
+	echo "Hello World!";
 
-echo "Hello World!";
-
-echo "<br/>";
-
-echo "Youyou";
+	echo "<br/>";
+	echo "YouYou ";
+	echo "Youyou";
+echo '</p>';
+echo '</body>';
+echo '</html>';
 ?>
